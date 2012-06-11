6476a7fcf3eb90d10c6d51c5f29c41d0243afced
Fredrik Strandin <fredrik@strandin.name>
2012-06-11 14:51:26 +0200

    Added some initial code for configuration-handling.

--- src/se/kd35a/seldon/SeldonMain.java
+++ src/se/kd35a/seldon/SeldonMain.java
@@ -1,10 +1,65 @@
 package se.kd35a.seldon;
 
-public class SeldonMain {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class SeldonMain extends Thread {
+	private static final String CONFIG_FILE = "config.ini";
+	private static final String CONFIG_KEY_SERVER	= "server";
+	private static final String CONFIG_KEY_PORT		= "port";
+	private static final String CONFIG_KEY_CHANNEL	= "channel";
+	private Properties config;
 	
 	public static void main(String[] args) {
-		System.out.println("Not yet implemented.");
-		System.exit(1);
+		new SeldonMain().start();
+	}
+	
+	public SeldonMain() {
+		config = new Properties();
+	}
+	
+	public void run() {
+		loadConfiguration();
+		checkConfiguration();
+	}
+
+	private void loadConfiguration() {
+		try {
+			config.load(new FileReader(CONFIG_FILE));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("File not found: " + CONFIG_FILE);
+			System.err.println("Please create a configuration-file before " +
+					"proceeding.");
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("An IO error occured while trying to read " +
+					CONFIG_FILE);
+			System.exit(2);
+		}
+	}
+
+	private void checkConfiguration() {
+		boolean error = false;
+		
+		String[] confs =
+			{CONFIG_KEY_SERVER,
+			 CONFIG_KEY_PORT,
+			 CONFIG_KEY_CHANNEL};
+		for (String conf : confs) {
+			if (config.getProperty(conf) == null) {
+				error = true;
+				System.err.println("Could not load property '" + conf +
+						"' from configuration-file " + CONFIG_FILE);
+			}
+		}
+		
+		if (error) {
+			System.exit(3);
+		}
 	}
 	
 }
